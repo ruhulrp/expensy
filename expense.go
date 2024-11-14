f4202e4abea2e78056989d5c0835d445f1277646
Ruhul Amin <ruhulrp71@gmail.com>
2024-11-14 23:43:40 +0600

    Version: 0.1.0

--- expense.go
+++ expense.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -105,6 +106,33 @@ func appendExpense(filename string, newExpense Expense) error {
 	return nil
 }
 
+func deleteExpense(id int) error {
+
+	filename := "expense.json"
+
+	var x []Expense
+
+	exp, err := readExpense(filename)
+
+	if err != nil {
+		return err
+	}
+
+	for i, e := range exp {
+
+		if e.ID != id {
+			x = append(x, exp[i])
+		}
+	}
+
+	err = writeExpense(x, filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func expensesReport() {
 	exp, _ := readExpense("expense.json")
 
@@ -129,6 +157,92 @@ func expensesReport() {
 
 	table.Render()
 
+}
+
+func saveUpdatedInfo(id int) error {
+	filename := "expense.json"
+
+	updateExp := getUpdateInfo(id)
+
+	expenses, err := readExpense(filename)
+
+	if err != nil {
+		return err
+	}
+
+	expenses = append(expenses, updateExp)
+
+	err = writeExpense(expenses, filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getUpdateInfo(id int) Expense {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	var choice int
+
+	x := findExpenseByID(id)
+
+	///////////////////////////////////
+
+	fmt.Println("What do you edit?\n1. Date\n2. Category\n3. Item\n4. Price\n5. Description")
+	fmt.Scan(&choice)
+
+	///////////////////////////////
+
+	switch {
+	case choice == 1:
+		fmt.Println("Edit Date: " + x.Date)
+		fmt.Scan(&x.Date)
+
+	case choice == 2:
+		fmt.Print("Edit Category: " + x.Category)
+		category, _ := reader.ReadString('\n')
+		x.Category = category
+
+	case choice == 3:
+		fmt.Print("Edit Item: " + x.Item)
+		item, _ := reader.ReadString('\n')
+		x.Item = item
+
+	case choice == 4:
+		fmt.Printf("Edit Price: %v\n", x.Price)
+		fmt.Scan(&x.Price)
+
+	case choice == 5:
+		fmt.Print("Edit Description: " + x.Description)
+		descripton, _ := reader.ReadString('\n')
+		x.Description = descripton
+	}
+
+	/////////////////////////
+
+	fmt.Println("You updated the Expense!")
+
+	deleteExpense(id)
+
+	return x
+
+}
+
+func findExpenseByID(id int) Expense {
+
+	var data Expense
+
+	exp, _ := readExpense("expense.json")
+
+	for _, ex := range exp {
+		if ex.ID == id {
+			data = ex
+		}
+	}
+
+	return data
 }
 
 func fileExist(filename string) bool {
--- main.go
+++ main.go
@@ -36,12 +36,26 @@ func main() {
 		fmt.Print(expense.addExpense())
 
 	case choice == 2:
+
+		var n int
+
+		fmt.Println("Give an ID: ")
+		fmt.Scan(&n)
+
+		saveUpdatedInfo(n)
+
 		fmt.Println("You successfully updated an expense!")
 	case choice == 3:
+		var n int
+
+		fmt.Println("Give an ID: ")
+		fmt.Scan(&n)
+
+		deleteExpense(n)
+
 		fmt.Println("You successfully deleted an expense!")
 	case choice == 4:
-
-		fmt.Println("Here is your Report!")
+		fmt.Println("Here is your Report:")
 		expensesReport()
 	}
 
